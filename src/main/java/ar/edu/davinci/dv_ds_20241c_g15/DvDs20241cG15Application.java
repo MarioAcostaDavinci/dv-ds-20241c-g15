64f2109496757cb4940e9a264b7136dfdeaccb51
MarioCode <mario.acosta@davinci.edu.ar>
2024-06-11 12:33:42 -0300

    v1.00
    reemplazo de carpeta por spring initializer

---
+++ src/main/java/ar/edu/davinci/dv_ds_20241c_g15/DvDs20241cG15Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dv_ds_20241c_g15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20241cG15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20241cG15Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dv_ds_20241c_g15/DvDs20241cG15ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dv_ds_20241c_g15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20241cG15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
